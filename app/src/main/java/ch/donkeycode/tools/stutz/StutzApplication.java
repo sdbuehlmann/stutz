806f44ee75fc163c9d04c214d40708fde48d0b98
Simon Bühlmann <sdbuehlmann@gmail.com>
2024-12-07 11:36:56 +0100

    Finish setup

---
+++ app/src/main/java/ch/donkeycode/tools/stutz/StutzApplication.java
@@ -0,0 +1,13 @@
+package ch.donkeycode.tools.stutz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StutzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StutzApplication.class, args);
+    }
+
+}
